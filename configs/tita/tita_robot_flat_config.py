6ed98a30f0a70f345ca1c12e6de774f249d22869
liu kexin <liukexin@directdrivetech.com>
2025-03-28 13:35:20 +0800

    tita balance in 3000pt

--- configs/tita/tita_robot_flat_config.py
+++ configs/tita/tita_robot_flat_config.py
@@ -32,31 +32,15 @@ from configs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
 class TitaRobotFlatCfg( LeggedRobotCfg ):
     class env(LeggedRobotCfg.env):
-        num_envs = 4096
         num_actions = 8
         n_scan = 187
-        n_priv_latent =  4 + 1 + 8 + 8 + 8 + 6 + 1 + 2 + 1 - 3 + 3 - 3 + 4 - 7
-        n_proprio = 33 + 3
+        n_priv_latent =  2 + 1 + 4 + 1 + 1 + 8 + 8 + 8
+        n_proprio = 36
         history_len = 10
         num_observations = n_proprio + n_scan + history_len*n_proprio + n_priv_latent
 
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.49] # x,y,z [m]
-        """
-          unitree go2 sdk order:
-               -0.1 <-3 FR_hip_joint 0 -> 0.0
-               0.8 <- 4 FR_thigh_joint 1 -> 0.9
-               -1.5 <- 5 FR_calf_joint 2 -> -1.8
-               0.1 <- 0 FL_hip_joint 3 -> 0.0
-               0.8 <- 1 FL_thigh_joint 4 -> 0.9
-               -1.5 <- 2 FL_calf_joint 5 -> -1.8
-               -0.1 <- 9 RR_hip_joint 6 -> 0.0
-               1 <- 10 RR_thigh_joint 7 -> 0.9
-               -1.5 <- 11 RR_calf_joint 8 -> -1.8
-               0.1 <- 6 RL_hip_joint 9 -> 0.0
-               1 <- 7 RL_thigh_joint 10 -> 0.9
-               -1.5 <- 8 RL_calf_joint 11 -> -1.8
-        """
+        pos = [0.0, 0.0, 0.4] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
                 'joint_left_leg_1': 0,
                 'joint_right_leg_1': 0,
@@ -71,45 +55,36 @@ class TitaRobotFlatCfg( LeggedRobotCfg ):
                 'joint_right_leg_4': 0,
         }
 
-        start_joint_angles = { # = target angles [rad] when stand still
-                'joint_left_leg_1': 0,
-                'joint_right_leg_1': 0,
-
-                'joint_left_leg_2': 0.8,
-                'joint_right_leg_2': 0.8,
-
-                'joint_left_leg_3': -1.5,
-                'joint_right_leg_3': -1.5,
-
-                'joint_left_leg_4': 0,
-                'joint_right_leg_4': 0,
-        }
-
-
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
-        control_type = 'P'
-        stiffness = {'joint': 40.}  # [N*m/rad]
-        damping = {'joint': 1.0}     # [N*m*s/rad]
+        control_type = 'P_AND_V'
+        stiffness = {'leg_1': 30.,
+                     'leg_2': 30.,
+                     'leg_3': 30.,
+                     'leg_4': 10.}  # [N*m/rad]
+        damping = {'leg_1': 1.0,
+                   'leg_2': 1.0,
+                   'leg_3': 1.0,
+                   'leg_4': 0.5}     #  [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.25
+        foot_scale_reduction = 8.0
         # decimation: Number of control action updates @ sim DT per policy DT
         decimation = 4
         hip_scale_reduction = 0.5
-
         use_filter = True
 
     class commands( LeggedRobotCfg.control ):
-        curriculum = True
+        curriculum = False
         max_curriculum = 1.0
         num_commands = 4  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
         resampling_time = 10.  # time before command are changed[s]
-        heading_command = True  # if true: compute ang vel command from heading error
+        heading_command = False  # if true: compute ang vel command from heading error
         global_reference = False
 
         class ranges:
             lin_vel_x = [-0.5, 0.5]  # min max [m/s]
-            lin_vel_y = [-0.5, 0.5]  # min max [m/s]
+            lin_vel_y = [-0, 0]  # min max [m/s]
             ang_vel_yaw = [-1, 1]  # min max [rad/s]
             heading = [-3.14, 3.14]
 
@@ -134,39 +109,47 @@ class TitaRobotFlatCfg( LeggedRobotCfg ):
         penalize_contacts_on = ["leg_3"]
         terminate_after_contacts_on = ["base"]
         self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
-        flip_visual_attachments = False
   
     class rewards( LeggedRobotCfg.rewards ):
+        only_positive_rewards = False
         soft_dof_pos_limit = 0.9 
-        # soft_dof_vel_limit = 0.9
-        # soft_torque_limit = 0.9
-        # base_height_target = 0.34
-        # clearance_height_target = -0.24
-
-        base_height_target = 0.31
-        clearance_height_target = -0.22
-
-        only_positive_rewards = True
+        base_height_target = 0.4
+        min_feet_distance = 0.57
+        max_feet_distance = 0.60
+        # tracking_sigma = 0.1
         class scales( LeggedRobotCfg.rewards.scales ):
-            # foot_clearance = -1
-            # foot_mirror = -0.1
-            # foot_slide = -0.1
+            lin_vel_z = 0.0 # off
+            ang_vel_xy = 0.0 # off
+            orientation = -0.2 # 很重要，不加的话会导致存活时间下降
+            base_height = -1.0
+            torques = -2.5e-05
+            dof_vel = 0.0 # off
+            dof_acc = -2.5e-07
+            action_rate = -0.01
+            collision = -10.0
+            termination = 0.0 # off
+            dof_pos_limits = -2.0
+            torque_limits = 0.0 # off
+            tracking_lin_vel = 1.0
+            tracking_ang_vel = 0.5 # off
+            feet_air_time = 0.0 # off
+            # no_fly = 1.0
+            # stand_still = -1.0
+            # feet_contact_forces = 0.0 # off
+            # feet_distance = -100
+            # survival = 0.1
+            # wheel_adjustment = 1.0
+            # leg_symmetry = 10.0
 
-            foot_clearance = -0.
-            foot_mirror = -0.0
-            foot_slide = -0.0
-            collision = -1
-            base_height = -2.0
-            stumble = -0.05
-            com_feet = 1.0
-            torques = -1e-5
+            hip_pos = 0.25
+            foot_mirror = 0.15
 
 
     class domain_rand( LeggedRobotCfg.domain_rand):
         randomize_friction = True
         #friction_range = [0.2, 2.75]
         friction_range = [0.2, 1.25]
-        randomize_restitution = False
+        randomize_restitution = True
         restitution_range = [0.0,1.0]
         randomize_base_mass = True
         #added_mass_range = [-1., 3.]
@@ -180,7 +163,7 @@ class TitaRobotFlatCfg( LeggedRobotCfg ):
         randomize_motor = True
         motor_strength_range = [0.9, 1.1]
 
-        randomize_kpkd = True
+        randomize_kpkd = False
         kp_range = [0.9,1.1]
         kd_range = [0.9,1.1]
 
@@ -194,65 +177,16 @@ class TitaRobotFlatCfg( LeggedRobotCfg ):
         # randomize_initial_joint_pos = True
         # initial_joint_pos_range = [0.5, 1.5]
     
-    class depth( LeggedRobotCfg.depth):
-        use_camera = False
-        camera_num_envs = 192
-        camera_terrain_num_rows = 10
-        camera_terrain_num_cols = 20
-
-        position = [0.27, 0, 0.03]  # front camera
-        angle = [-5, 5]  # positive pitch down
-
-        update_interval = 1  # 5 works without retraining, 8 worse
-
-        original = (106, 60)
-        resized = (87, 58)
-        horizontal_fov = 87
-        buffer_len = 2
-        
-        near_clip = 0
-        far_clip = 2
-        dis_noise = 0.0
-        
-        scale = 1
-        invert = True
-    
     class costs:
         num_costs = 3
         class scales:
             pos_limit = 0.1
             torque_limit = 0.1
             dof_vel_limits = 0.1
-            #foot_slide = 1
-            #foot_nocontact_regular = 1
-            #feet_air_time = 1
-            #foot_mirror = 0.1
-            # trot_contact=0.1
-            # stand_still=0.1
-            # #idol_contact = 0.1
-            # #idol_contact = 0.1
-            #base_height = 0.1
-            # foot_swing_clearance = 1
-            #acc_smoothness = 0.1
-
         class d_values:
             pos_limit = 0.0
             torque_limit = 0.0
             dof_vel_limits = 0.0
-            #foot_slide = 0.0
-            #foot_nocontact_regular = 0.0
-            #feet_air_time = 0.0
-            #foot_mirror = 2.0
-            # trot_contact= 5.0
-            # stand_still = 0.0
-            # #idol_contact = 0.0
-            # #idol_contact = 0.0
-            #base_height = 0.0
-            # foot_swing_clearance = 0.0
-            #acc_smoothness = 2.0
-
-    class cost:
-        num_costs = 3
     
     class terrain(LeggedRobotCfg.terrain):
         mesh_type = 'plane'  # "heightfield" # none, plane, heightfield or trimesh
@@ -263,16 +197,16 @@ class TitaRobotFlatCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
         entropy_coef = 0.01
         learning_rate = 1e-3
-        max_grad_norm = 1
+        max_grad_norm = 0.01
         num_learning_epochs = 5
         num_mini_batches = 4 # mini batch size = num_envs*nsteps / nminibatches
-        cost_value_loss_coef = 1
-        cost_viol_loss_coef = 1
+        cost_value_loss_coef = 0.1
+        cost_viol_loss_coef = 0.1
 
     class policy( LeggedRobotCfgPPO.policy):
         init_noise_std = 1.0
         continue_from_last_std = True
-        scan_encoder_dims = None#[128, 64, 32]
+        scan_encoder_dims = [128, 64, 32]
         actor_hidden_dims = [512, 256, 128]
         critic_hidden_dims = [512, 256, 128]
         #priv_encoder_dims = [64, 20]
@@ -291,11 +225,11 @@ class TitaRobotFlatCfgPPO( LeggedRobotCfgPPO ):
       
     class runner( LeggedRobotCfgPPO.runner ):
         run_name = ''
-        experiment_name = 'tita_rouge'
+        experiment_name = 'tita_flat'
         policy_class_name = 'ActorCriticBarlowTwins'
         runner_class_name = 'OnConstraintPolicyRunner'
         algorithm_class_name = 'NP3O'
-        max_iterations = 10000
+        max_iterations = 3000
         num_steps_per_env = 24
         resume = False
         resume_path = ''
